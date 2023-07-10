2a2c93bbcf025a90e90db9e1b7163292c9c21203
Martin2504 <58813528+Martin2504@users.noreply.github.com>
2023-07-10 23:01:32 +0100

    Arrays

--- start.py
+++ start.py
@@ -1,4 +1,26 @@
 import numpy as np
 
-a = np.array([1,2,3])
+a = np.array([1,2,3])   # 1D array
 print(a)
+
+b = np.array([[9.0, 8.0, 7.0], [6.0, 5.0, 4.0]])    # 2D array
+print(b)
+
+# Get dimension
+print(a.ndim)
+print(b.ndim)
+
+# Get Shape
+print(b.shape)
+
+# Get Type
+print(a.dtype)
+
+c = np.array([4,5,6], dtype ='int16')   # Specifying array size in bits
+print(c.dtype)
+
+# Get size in bytes
+print(a.itemsize)   
+
+# Get number of elements
+print(a.size)
