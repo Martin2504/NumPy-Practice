d91c4478fee726a9f2a027f1c3e2b0c42e48ad3c
Martin2504 <58813528+Martin2504@users.noreply.github.com>
2023-07-12 22:30:37 +0100

    Initializing different types of arrays

---
+++ arrays.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# Initializing different types of arrays.
+
+# All 0s matrix.
+a = np.zeros(5)
+print(a)
+print(np.zeros((2,3,3)))
+
+# All 1s matrix.
+print(np.ones((4,2,2)))
+
+# Any other number.
+print(np.full((2,2), 99))
+
+# Any other number (full_like)
+print(np.full_like(a, 4))       # Replace the 0's with 4's in a. 
+
+# Random decimal numbers matrix. 
+print(np.random.rand(4,2))
+print(np.random.randint(7, size=(3,3))) # Random int's
+print(np.random.random_sample(a.shape)) # Replace the 0's with ramdp, decimals in a. 
